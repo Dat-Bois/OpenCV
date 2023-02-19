288e2320c6c8af4178f8a48ae02d1ff41f4deee6
Eesh <pc@MacBookPro-3.local>
2023-02-18 18:53:30 -0800

    added bounding boxes

--- color_selector.py
+++ color_selector.py
@@ -5,32 +5,31 @@ import math
 import time
 
 #cap = cv2.VideoCapture(0)
-#cap = cv2.VideoCapture("/Users/pc/Desktop/Code/OpenCV/gateSample_01.mov")
+cap = cv2.VideoCapture("/Users/pc/Desktop/Code/OpenCV/gateSample_01.mov")
 
-cap = cv2.VideoCapture(r"C:\Users\Eesh\Documents\Robotics\OpenCV\gateSample_01.mov")
+#cap = cv2.VideoCapture(r"C:\Users\Eesh\Documents\Robotics\OpenCV\gateSample_01.mov")
+
 
 def drawBox(maskFrame, frameOG):
     font = cv2.FONT_HERSHEY_SIMPLEX
-    cnts = cv2.findContours(
-        maskFrame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+    cnts = cv2.findContours(maskFrame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
     values = [(0, 0), (0, 0), (0, 0), (0, 0), (0, 0), 0]
-    if len(cnts) > 0:
-        c = max(cnts, key=cv2.contourArea)
-        area = cv2.contourArea(c)
+    #if len(cnts) > 0:
+    for i in range(len(cnts)):
+        #c = max(cnts, key=cv2.contourArea)
+        area = cv2.contourArea(cnts[i])
+        c=cnts[i]
         # print(area)
-        if(area > 20):
+        if(area > 300): #20
             ((x, y), radius) = cv2.minEnclosingCircle(c)
             x2, y2, w, h = cv2.boundingRect(c)
             cv2.rectangle(frameOG, (x2, y2), (x2+w, y2+h), (0, 255, 0), 2)
-            cv2.circle(frameOG, (int(x), int(y)), int(
-                radius), (255, 255, 255), 5, 2)
-            values = [(x2, y2), (x2+w, y2), (x2, y2+h),
-                      (x2+w, y2+h), (int(x), int(y)), int(radius)]
+            #cv2.circle(frameOG, (int(x), int(y)), int(radius), (255, 255, 255), 5, 2)
+            values = [(x2, y2), (x2+w, y2), (x2, y2+h), (x2+w, y2+h), (int(x), int(y)), int(radius)]
             # print(values)
         else:
             values = [(0, 0), (0, 0), (0, 0), (0, 0), (0, 0), 0]
-    cv2.putText(frameOG, str(values), (10, 30), font,
-                0.5, (255, 255, 255), 2, cv2.LINE_AA)
+    cv2.putText(frameOG, str(values), (10, 30), font, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
     cv2.imshow("Result", frameOG)
 
 
@@ -86,7 +85,7 @@ def get_color(frame):
     mask = cv2.inRange(hsv, colorLower, colorUpper)
     #mask2 = cv2.inRange(hsv, colorLower2, colorUpper2)
     mask_final = mask  # + mask2
-    cv2.imshow("mask", mask_final)
+    #cv2.imshow("mask", mask_final)
     kernel = np.ones((3, 3), np.uint8)
     eroded = cv2.erode(mask_final, kernel, iterations=0)
     #dilated = cv2.dilate(mask_final, kernel, iterations=3)
@@ -111,13 +110,21 @@ cv2.createTrackbar("HueUpper", "HSV Picker", 0, 360, on_trackbarHu)
 cv2.createTrackbar("SatUpper", "HSV Picker", 0, 255, on_trackbarSu)
 cv2.createTrackbar("ValUpper", "HSV Picker", 0, 255, on_trackbarVu)
 
+on_trackbarHl(0)
+on_trackbarSl(64)
+on_trackbarVl(0)
+on_trackbarHu(17)
+on_trackbarSu(226)
+on_trackbarVu(222)
+
+
 while (True):
     ret, frame = cap.read()
     if ret == True:
         cv2.imshow("OG", frame)
         cord = get_color(frame)
-        #drawBox(cord[0], cord[1])
-        time.sleep(1/30)
+        drawBox(cord[0], cord[1])
+        time.sleep(1/60)
 
     # -----------------
     if cv2.waitKey(1) & 0xFF == ord('q'):
