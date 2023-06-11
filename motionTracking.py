1f60e28f023da519fb89b249dbd8b42bd7224a08
Eesh Vij <eeshvij@Eeshs-MacBook-Pro.local>
2023-06-11 15:56:09 -0700

    Basic implementation works, need to add perceptive confidencew

--- motionTracking.py
+++ motionTracking.py
@@ -1,15 +1,21 @@
 import cv2
 import numpy as np
 import math
-
+import time
 
 ret = True
-cap = cv2.VideoCapture('sample.mov')
+cap = cv2.VideoCapture('sample.mp4')
 
 width = 640
 height = 480
+edgeIgnore = 40
+centerPoints_prev = []
+trackingObjects = {}
+track_id = 0
+count=0
+position = [height,height]
 
-def processImage(frame):
+def processImage(frame, cnt):
     dim = (width, height)
     resized = cv2.resize(frame, dim)
     gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
@@ -20,31 +26,84 @@ def processImage(frame):
     #blur = cv2.bilateralFilter(contrast,9,75,75)
     ret, binary = cv2.threshold(blur, 200, 255, cv2.THRESH_BINARY)
     invert = cv2.bitwise_not(binary)
+    #cv2.imshow('binary', binary)
+    #cv2.imshow('contrast', contrast)
+    return motionTrack(resized, invert, cnt)
 
-    contours, hierarchy = cv2.findContours(invert, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) #TREE
-    for cnt, contour in enumerate(contours):
+def motionTrack(frame, processed_frame, cnt):
+    global centerPoints_prev
+    global track_id
+    global trackingObjects
+    global position
+    tempImg = np.zeros((height*2, width*2, 3), dtype = np.uint8)
+    counter = 1
+    sumX = sumY = 0
+    centerPoints_curr = []
+    contours, hierarchy = cv2.findContours(processed_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) #TREE
+    for cnts, contour in enumerate(contours):
              area = cv2.contourArea(contour)
-             rect = cv2.minAreaRect(contour)
-             box = cv2.boxPoints(rect)
-             box = np.intp(box)
-             x, y, w, h = cv2.boundingRect(contour)
              if(area>10 and area<3000):
-                cv2.drawContours(resized,[box],0,(255,0,0),2)
-                cv2.putText(resized, str(area), (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
-    cv2.imshow('binary', binary)
-    cv2.imshow('contrast', contrast)
-    return resized
+                rect = cv2.minAreaRect(contour)
+                box = cv2.boxPoints(rect)
+                box = np.intp(box)
+                x, y, w, h = cv2.boundingRect(contour)
+                cx = int((x+x+w)/2)
+                cy = int((y+y+h)/2)
+                if(not ((cx<edgeIgnore or cx>width-edgeIgnore) or (cy<edgeIgnore or cy>height-edgeIgnore))):
+                    centerPoints_curr.append((cx,cy))
+                cv2.drawContours(frame,[box],0,(255,0,0),2)
+                #cv2.putText(resized, str(area), (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+    if(cnt<2):
+        for pts in centerPoints_curr:
+            for pts2 in centerPoints_prev:
+                distance = math.hypot(pts2[0]-pts[0], pts2[1]-pts[1])
+                if(distance<15):
+                    trackingObjects[track_id] = pts
+                    track_id+=1
+    else:
+        centerPoints_curr_copy = centerPoints_curr.copy()
+        trackingObjects_copy = trackingObjects.copy()
+        for object_id, pts2 in trackingObjects_copy.items():
+            objectExist = False
+            for pts in centerPoints_curr_copy:
+                   distance = math.hypot(pts2[0]-pts[0], pts2[1]-pts[1])
+                   if(distance<10):
+                      sumX += pts2[0]-pts[0]
+                      sumY += pts2[1]-pts[1]
+                      counter+=1
+                      trackingObjects[object_id] = pts
+                      objectExist = True
+                      if pts in centerPoints_curr:
+                        centerPoints_curr.remove(pts)
+                      continue
+            if not objectExist:
+                trackingObjects.pop(object_id)
 
+        for pts in centerPoints_curr:
+            trackingObjects[track_id] = pts
+            track_id+=1
+
+    for object_id, pt in trackingObjects.items():
+         cv2.circle(frame, pt, 5, (0,0,255),-1)
+         cv2.putText(frame, str(object_id), (pt[0], pt[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+
+    position[0] = position[0] + int(sumX/counter)
+    position[1] = position[1] + int(sumY/counter)
+    cv2.circle(tempImg, position, 5, (0,0,255),-1)
+    cv2.imshow('pos', tempImg)
+    centerPoints_prev = centerPoints_curr.copy()
+    return frame
 
 while(ret):
     ret, frame = cap.read()
     try: 
-        cv2.imshow('result', processImage(frame))
-    except:
+        cv2.imshow('result', processImage(frame, count))
+        time.sleep(1/30)
+        count+=1
+    except Exception as e:
+         print(e)
          pass
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cap.release()
 cv2.destroyAllWindows()
-
-#https://youtu.be/GgGro5IV-cs
